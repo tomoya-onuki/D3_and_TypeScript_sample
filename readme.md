46f0f3f483812149831336279400d0aca829f6a2
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-01-16 18:20:53 +0900

    stream graphの論文を読んだ

--- readme.md
+++ readme.md
@@ -10,6 +10,6 @@
 - 新型コロナウイルスの**関東地方における新規陽性者数の日別推移データ**をStreamGraphで可視化する。
 - 関東地方は東京都、埼玉県、神奈川県、千葉県、群馬県、栃木県、茨城県の7県とする。
 - データ入手先: https://www.mhlw.go.jp/stf/covid-19/open-data.html
-- 参考文献 : L. Byron and M. Wattenberg, Stacked Graphs Geometry & Aesthetics, IEEE TVCG, Vol. 14, No. 6, pp. 1245-1252, 2008.
+- 参考文献 : L. Byron and M. Wattenberg, Stacked Graphs - Geometry & Aesthetics, IEEE TVCG, Vol. 14, No. 6, pp. 1245-1252, 2008.
 
 <img src="./img/1f_StreamGraph.png" width="50%">
--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,8 @@ function draw(data: DSVRowArray): void {
     // 使うキー
     const keys: string[] = ["Tokyo", "Saitama", "Kanagawa", "Chiba", "Tochigi", "Gunma", "Ibaraki"];
     const stakedData: any = d3.stack()
-        .offset(d3.stackOffsetSilhouette)
+        // .offset(d3.stackOffsetWiggle)
+        .offset(d3.stackOffsetSilhouette) // これでいいはず
         .keys(keys)(<any>data);
 
     // 一番上に積み上げられたデータの最大値,最小値
@@ -68,8 +69,8 @@ function draw(data: DSVRowArray): void {
 
     const colorScale = d3.scaleOrdinal()
         .domain(keys)
-        .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628']);
-
+        .range(["#007FB1", "#3261AB", "#009F8C", "#6A8CC7", "#44A5CB", "#40BFB0", "#99CFE5"]);
+        
     // 面を作成
     const area: any = d3.area()
         .x((d: any) => xScale(new Date(String(d.data.Date))))
@@ -81,8 +82,10 @@ function draw(data: DSVRowArray): void {
         .data(stakedData)
         .join("path")
         .style("fill", (d: any) => colorScale(d.key))
+        .style("stroke", (d: any) => colorScale(d.key))
         .attr("transform", "translate(" + marginLeft + "," + marginTop + ")")
         .attr("d", area);
 
 
 }
+
