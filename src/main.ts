a976521416e176a1240dd988c49a7c4ac975fef1
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-01-17 17:53:57 +0900

    3rd

--- src/main.ts
+++ src/main.ts
@@ -37,18 +37,16 @@ function draw(data: DSVRowArray): void {
         .attr("width", width)
         .attr("height", height);
 
-    
+    // 積み上げデータの生成
     // 使うキー
     const keys: string[] = ["Tokyo", "Saitama", "Kanagawa", "Chiba", "Tochigi", "Gunma", "Ibaraki"];
-    // const braidedData
-    data.forEach((d: DSVRowString, idx: number) => {
-        keys.forEach(key => {
-            d[key];
-        });
-    });
+    const colorScale = d3.scaleOrdinal()
+        .domain(keys)
+        .range(["#007FB1", "#3261AB", "#009F8C", "#6A8CC7", "#44A5CB", "#40BFB0", "#99CFE5"]);
 
-    
-    
+
+    const keysList: string[][] = blaided(data, keys);
+
 
     // x axis
     let xScale: any = d3.scaleTime()
@@ -69,25 +67,79 @@ function draw(data: DSVRowArray): void {
     yLabel.selectAll("text")
         .attr("display", "none");
 
-    const colorScale = d3.scaleOrdinal()
-        .domain(keys)
-        .range(["#007FB1", "#3261AB", "#009F8C", "#6A8CC7", "#44A5CB", "#40BFB0", "#99CFE5"]);
 
-    // 面を作成
-    const area: any = d3.area()
-        .x((d: any) => xScale(new Date(String(d.data.Date))))
-        .y0((d: any) => yScale(Number(d[0])))
-        .y1((d: any) => yScale(Number(d[1])));
+
+
+    // keys.forEach((key: string) => {
+
+    // });
+    const area: any[] = [];
 
     // 描画
-    svg.selectAll("mylayers")
-        .data(data)
-        .join("path")
-        .style("fill", (d: any) => colorScale(d.key))
-        .style("stroke", (d: any) => colorScale(d.key))
+    let path: any = svg.append("path")
+        .datum(keysList)
         .attr("transform", "translate(" + marginLeft + "," + marginTop + ")")
-        .attr("d", area);
+        .style("fill", "none")
+        // .style("stroke", (d: any) => colorScale(d.key))
+        .style("stroke", "#00a0c")
+        .attr("stroke-width", 1.5)
+        .attr("d", area)
 
+
+    // keysList.forEach((keys: string[], i: number) => {
+    //     if (i < keysList.length - 1) {
+    //         const area: any[] = [];
+
+    //         keys.forEach((key: string) => {
+    //             // 面を作成
+    //             area.push(d3.area()
+    //                 .x(xScale(data[i].Date))
+    //                 .y0(0)
+    //                 .y1(yScale(Number(data[i][key]))));
+    //             // area.push(d3.area()
+    //             //     .x(xScale(data[i+1].Date))
+    //             //     .y0(0)
+    //             //     .y1(yScale(Number(data[i+2][key]))));
+    //         });
+
+    //         // 描画
+    //         let path: any = svg.append("path")
+    //             .attr("transform", "translate(" + marginLeft + "," + marginTop + ")")
+    //             .style("fill", "none")
+    //             // .style("stroke", (d: any) => colorScale(d.key))
+    //             .style("stroke", "#00a0c")
+    //             .attr("stroke-width", 1.5)
+    //             .attr("d", area)
+    //     }
+    // });
 
 }
 
+
+
+function blaided(data: DSVRowArray, keys: string[]): string[][] {
+    let keysList: string[][] = [];
+    data.forEach((d: any) => {
+
+        // keyを昇順にソートする
+        for (let i = 0; i < keys.length - 1; i++) {
+            for (let j = 1; j < keys.length - i + 1; j++) {
+                if (Number(d[keys[j - 1]]) > Number(d[keys[j]])) {
+                    let tmp: string = keys[j - 1];
+                    keys[j - 1] = keys[j];
+                    keys[j] = tmp;
+                }
+            }
+        }
+
+        keysList.push(keys);
+    });
+    // keysList.forEach((keys, i) => {
+    //     keys.forEach(key => {
+    //         console.log(key, data[i][key]);
+    //         // console.log(key, data[i][key]);
+    //     });
+    // });
+
+    return keysList;
+}
