c692c103c147b24f4f705ff3c24c384890f51d75
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-01-18 23:18:04 +0900

    ラベルのレイアウトを手動で調整

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ window.addEventListener('load', () => {
     init();
 }, false);
 
-let width: number = 600;
-let height: number = 70;
-let marginTop: number = 20;
-let marginRight: number = 120;
+let width: number = 500;
+let height: number = 100;
+let marginTop: number = 10;
+let marginRight: number = 20;
 let marginBottom: number = 20;
 let marginLeft: number = 10;
 
@@ -32,7 +32,7 @@ function draw(data: DSVRowArray): void {
     let chartHeight: number = height - marginTop - marginBottom;
 
     const keys: string[] = Object.keys(data[0]);
-    // const keys: string[] = ["Tokyo", "Saitama", "Kanagawa", "Chiba", "Tochigi", "Gunma", "Ibaraki"];
+    // const keys: string[] = ["Tokyo", "Saitama", "Kanagawa", "Chiba", "Aichi", "Osaka", "Fukuoka", "Okinawa"];
 
     // 最大値を求める (これをしないと縦軸のスケールが揃わない)
     let max: number = 0;
@@ -48,6 +48,7 @@ function draw(data: DSVRowArray): void {
     const vAxisMax: number = 500; // 縦軸のメモリの上限
     const div: number = Math.floor(max / vAxisMax); // 分割数
 
+    let count: number = 0;
     keys.forEach((key: string) => {
         if (key != 'ALL' && key != 'Date') {
 
@@ -66,7 +67,7 @@ function draw(data: DSVRowArray): void {
                 .range([0, chartWidth]);
             svg.append("g")
                 .attr("transform", "translate(" + marginLeft + "," + Number(marginTop + chartHeight) + ")")
-                .call(d3.axisBottom(xScale).ticks(10).tickFormat(<any>d3.timeFormat("%y/%m/%d")));
+                .call(d3.axisBottom(xScale).ticks(10).tickFormat(<any>d3.timeFormat("%y/%m/%d")).tickSize(1));
 
 
             // y axis
@@ -78,26 +79,36 @@ function draw(data: DSVRowArray): void {
                 .call(d3.axisLeft(yScale).ticks(0).tickSize(0));
 
 
-
             horizonData.forEach((data0: any, i: number) => {
+
+                // カラーラベル
+                let lightness: string = String(90 - 90 * i / div);
+                let color: string = "hsl(225, 60%, " + lightness + "%)";
+                let vMargin: number = 0;
+                let hMargin: number = i;
+                if (i > div / 2) {
+                    vMargin = 10;
+                    hMargin = i - Math.floor(div/2);
+                }
+                if (count === 0) {
+                    svg.append("text")
+                        .attr("text-anchor", "end")
+                        .attr("y", marginTop + 10 + vMargin)
+                        .attr("x", marginLeft + 100 + 75 * hMargin)
+                        .attr("font-size", "10px")
+                        .attr("font-family", "Arial")
+                        .attr("fill", color)
+                        .text("・" + String(Math.floor(i * vAxisMax)) + " ~ " + String(Math.floor((i + 1) * vAxisMax)));
+                }
+
+                
+
                 // 面
                 const area: any = d3.area()
                     .x((d: any) => xScale(new Date(String(d.Date))))
                     .y0(yScale(0))
                     .y1((d: any) => yScale(Number(d[key])));
 
-                let lightness: string = String(90 - 90 * i / horizonData.length);
-                let color: string = "hsl(225, 60%, " + lightness + "%)";
-
-                // カラーラベル
-                svg.append("text")
-                    .attr("text-anchor", "start")
-                    .attr("y", marginTop + 10 * i)
-                    .attr("x", chartWidth + marginLeft + 20)
-                    .attr("font-size", "8px")
-                    .attr("font-family", "Arial")
-                    .attr("fill", color)
-                    .text("・"+String(Math.floor(i * vAxisMax)) + " ~ " + String(Math.floor((i + 1) * vAxisMax)));
 
                 // 描画
                 svg.append("path")
@@ -109,16 +120,15 @@ function draw(data: DSVRowArray): void {
                     .attr("d", area);
             });
 
+            count++;
             // ラベル
             svg.append("text")
                 .attr("text-anchor", "start")
-                .attr("y", 15)
-                .attr("x", 15)
+                .attr("y", marginTop + 10)
+                .attr("x", marginLeft + 5)
                 .attr("font-size", "10px")
                 .attr("font-family", "Arial")
                 .text(key);
-
-
         }
     });
 }
