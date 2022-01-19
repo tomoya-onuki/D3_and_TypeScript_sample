ce789d2e7b343b176d5632169bfe7d1ab1076786
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-01-19 14:05:56 +0900

    1st

--- readme.md
+++ readme.md
@@ -6,8 +6,9 @@
 
 # D3.js + TypeScriptによる可視化サンプル
 
-## 1i　Horizon　Graph
+## f1　Horizon　Graph
 - 新型コロナウイルスの**各県の新規陽性者数の日別推移データ**をHorizon GraphのSmall Multiplesで可視化する。
+- HorizonGraphの分割数をインタラクティブに変更できるようにする。
 - データ入手先: https://www.mhlw.go.jp/stf/covid-19/open-data.html
 
-<img src="./img/1i_HorizonGraph.png" width="100%">
+<img src="./img/f1_HorizonGraph.gif" width="100%">
--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,120 @@
 import * as d3 from 'd3';
-import { DSVRowArray, DSVRowString } from 'd3';
-
+import { DSVRowArray, DSVRowString, svg } from 'd3';
+import $ = require('jquery');
 
 window.addEventListener('load', () => {
-    init();
-}, false);
-
-let width: number = 600;
-let height: number = 60;
-let marginTop: number = 0;
-let marginRight: number = 20;
-let marginBottom: number = 20;
-let marginLeft: number = 10;
-
-function init(): void {
-
     // データの読み込み
     d3.csv("../data/newly_confirmed_cases_daily.csv")
         .then((data) => {
-            draw(data);
+            new App(data).init();
         })
         .catch((error) => {
             console.log(error)
         });
-}
+}, false);
 
 
-function draw(data: DSVRowArray): void {
 
-    let chartWidth: number = width - marginLeft - marginRight;
-    let chartHeight: number = height - marginTop - marginBottom;
+class App {
+    private div: number = 1;
+    private data: DSVRowArray;
 
-    const keys: string[] = Object.keys(data[0]);
-    // const keys: string[] = ["Tokyo", "Saitama", "Kanagawa", "Chiba", "Aichi", "Osaka", "Fukuoka", "Okinawa"];
+    private width: number = 600;
+    private height: number = 60;
+    private marginTop: number = 0;
+    private marginRight: number = 20;
+    private marginBottom: number = 20;
+    private marginLeft: number = 10;
 
-    // 最大値を求める (これをしないと縦軸のスケールが揃わない)
-    let max: number = 0;
-    keys.forEach(key => {
-        if (key != 'ALL' && key != 'Date') {
-            let tmp: number = Number(d3.max(data, (d) => +Number(d[key])));
-            if (tmp > max) {
-                max = tmp;
+    private keys: string[] = ["Tokyo", "Saitama", "Kanagawa", "Chiba", "Aichi", "Osaka", "Fukuoka", "Okinawa"];
+    // private keys: string[] = Object.keys(data[0]);
+
+    constructor(data: DSVRowArray) {
+        this.data = data;
+    }
+
+    public init(): void {
+        const me: App = this;
+        const svgList: any[] = [];
+
+        let max: number = 0;
+        me.keys.forEach(key => {
+            if (key != 'ALL' && key != 'Date') {
+                // 最大値を求める (これをしないと縦軸のスケールが揃わない)
+                let tmp: number = Number(d3.max(this.data, (d) => +Number(d[key])));
+                if (tmp > max) {
+                    max = tmp;
+                }
+
+                // SVGの設定
+                const svg: any = d3.select("#chart")
+                    .append("svg")
+                    .attr("id", key)
+                    .attr("width", this.width)
+                    .attr("height", this.height);
+                svgList.push(svg);
             }
-        }
-    });
+        });
 
-    const vAxisMax: number = 500; // 縦軸のメモリの上限
-    const div: number = Math.floor(max / vAxisMax); // 分割数
+        me.keys.forEach((key: string, i: number) => {
+            if (key != 'ALL' && key != 'Date') {
+                me.draw(svgList[i], key, max);
+            }
 
-    let count: number = 0;
-    keys.forEach((key: string) => {
+        });
+
+
+
+        $('#div_slider').on('input', function () {
+            $('#div_slider_label').text('分割数: ' + $(this).val());
+            me.div = Number($(this).val());
+            me.keys.forEach((key: string, i: number) => {
+                me.clear(svgList[i]);
+                me.draw(svgList[i], key, max);
+            });
+        });
+        $('#width_slider').on('input', function () {
+            $('#width_slider_label').text('横幅: ' + $(this).val() + "px");
+            me.width = Number($(this).val());
+            me.keys.forEach((key: string, i: number) => {
+                svgList[i].attr("width", me.width)
+                svgList[i].attr("height", me.height);
+                me.clear(svgList[i]);
+                me.draw(svgList[i], key, max);
+            });
+        });
+        $('#height_slider').on('input', function () {
+            $('#height_slider_label').text('縦幅: ' + $(this).val() + "px");
+            me.height = Number($(this).val());
+            me.keys.forEach((key: string, i: number) => {
+                svgList[i].attr("width", me.width)
+                svgList[i].attr("height", me.height);
+                me.clear(svgList[i]);
+                me.draw(svgList[i], key, max);
+            });
+        });
+    }
+
+    private clear(svg: any) {
+        svg.selectAll("g").remove();
+        svg.selectAll("text").remove();
+        svg.selectAll("path").remove();
+    }
+
+    private draw(svg: any, key: string, max: number): void {
+
+        let chartWidth: number = this.width - this.marginLeft - this.marginRight;
+        let chartHeight: number = this.height - this.marginTop - this.marginBottom;
+        const vAxisMax: number = Math.floor(max / this.div) // 縦軸のメモリの上限
+
+        const data = this.data;
+
+        // console.log("draw! ", this.div);
+
+        // keys.forEach((key: string) => {
         if (key != 'ALL' && key != 'Date') {
 
-            // SVGの設定
-            const svg: any = d3.select("body")
-                .append("svg")
-                .attr("width", width)
-                .attr("height", height);
-
-            let horizonData: any = horizon(data, div, vAxisMax, key);
+            let horizonData: any = this.horizon(data, this.div, vAxisMax, key);
 
             // x axis
             const term = <any>d3.extent(data, (d) => new Date(String(d.Date)));
@@ -67,41 +123,43 @@ function draw(data: DSVRowArray): void {
                 .range([0, chartWidth]);
 
             svg.append("g")
-                .attr("transform", "translate(" + marginLeft + "," + Number(marginTop + chartHeight) + ")")
-                .call(d3.axisBottom(xScale).ticks(12).tickFormat(<any>d3.timeFormat("%y/%m/%d")).tickSize(1));
+                .attr("transform", "translate(" + this.marginLeft + "," + Number(this.marginTop + chartHeight) + ")")
+                .call(d3.axisBottom(xScale).ticks(chartWidth/100).tickFormat(<any>d3.timeFormat("%y/%m/%d")).tickSize(1));
 
 
 
             // y axis
             let yScale: any = d3.scaleLinear()
-                .domain([0, max / div])
+                .domain([0, max / this.div])
                 .range([chartHeight, 0]);
             svg.append("g")
-                .attr("transform", "translate(" + marginLeft + "," + marginTop + ")")
+                .attr("transform", "translate(" + this.marginLeft + "," + this.marginTop + ")")
                 .call(d3.axisLeft(yScale).ticks(0).tickSize(0));
 
 
             horizonData.forEach((data0: any, i: number) => {
 
                 // カラーラベル
-                let lightness: string = String(90 - 90 * i / div);
+                let lightness: string = String(90 - 90 * i / this.div);
                 let color: string = "hsl(225, 60%, " + lightness + "%)";
                 let vMargin: number = 0;
                 let hMargin: number = i;
-                if (i > div / 2) {
+                if (i > this.div / 2) {
                     vMargin = 10;
-                    hMargin = i - Math.floor(div/2);
+                    hMargin = i - Math.floor(this.div / 2);
                 }
-                if (count === 0) {
+
+                if (key == this.keys[0]) {
                     svg.append("text")
                         .attr("text-anchor", "end")
-                        .attr("y", marginTop + 10 + vMargin)
-                        .attr("x", marginLeft + 100 + 75 * hMargin)
+                        .attr("y", this.marginTop + 10 + vMargin)
+                        .attr("x", this.marginLeft + 100 + 75 * hMargin)
                         .attr("font-size", "10px")
                         .attr("font-family", "Arial")
                         .attr("fill", color)
                         .text("・" + String(Math.floor(i * vAxisMax)) + " ~ " + String(Math.floor((i + 1) * vAxisMax)));
                 }
+
 
 
 
@@ -115,52 +173,52 @@ function draw(data: DSVRowArray): void {
                 // 描画
                 svg.append("path")
                     .datum(data0)
-                    .attr("transform", "translate(" + marginLeft + "," + marginTop + ")")
+                    .attr("transform", "translate(" + this.marginLeft + "," + this.marginTop + ")")
                     .attr("stroke", "none")
                     .attr("fill", color)
                     .attr("stroke-width", 1.5)
                     .attr("d", area);
             });
 
-            count++;
             // ラベル
             svg.append("text")
                 .attr("text-anchor", "start")
-                .attr("y", marginTop + 10)
-                .attr("x", marginLeft + 5)
+                .attr("y", this.marginTop + 10)
+                .attr("x", this.marginLeft + 5)
                 .attr("font-size", "10px")
                 .attr("font-family", "Arial")
                 .text(key);
         }
-    });
-}
-
-function horizon(data: DSVRowArray, div: number, vAxisMax: number, key: string): any {
-
-    // 初期化
-    let newData: any[][] = new Array(div);
-    for (let i = 0; i < div; i++) {
-        newData[i] = new Array(data[0].length);
+        // });
     }
 
-    data.forEach((d: any, i: number) => {
+    private horizon(data: DSVRowArray, div: number, vAxisMax: number, key: string): any {
 
-        let value: number = Number(d[key]);
-
-        for (let j = 0; j < div; j++) {
-            if (value < j * vAxisMax) {
-                newData[j][i] = { [key]: 0, Date: d.Date };
-            }
-            else if (j * vAxisMax <= value && value < (j + 1) * vAxisMax) {
-                newData[j][i] = { [key]: value - vAxisMax * j, Date: d.Date };
-            }
-            else if ((j + 1) * vAxisMax <= value) {
-                newData[j][i] = { [key]: vAxisMax, Date: d.Date };
-            }
+        // 初期化
+        let newData: any[][] = new Array(div);
+        for (let i = 0; i < div; i++) {
+            newData[i] = new Array(data[0].length);
         }
 
-    });
+        data.forEach((d: any, i: number) => {
 
-    // console.log(newData);
-    return newData;
+            let value: number = Number(d[key]);
+
+            for (let j = 0; j < div; j++) {
+                if (value < j * vAxisMax) {
+                    newData[j][i] = { [key]: 0, Date: d.Date };
+                }
+                else if (j * vAxisMax <= value && value < (j + 1) * vAxisMax) {
+                    newData[j][i] = { [key]: value - vAxisMax * j, Date: d.Date };
+                }
+                else if ((j + 1) * vAxisMax <= value) {
+                    newData[j][i] = { [key]: vAxisMax, Date: d.Date };
+                }
+            }
+
+        });
+
+        // console.log(newData);
+        return newData;
+    }
 }
